4029a90d2a6dbd76e4048b287cdac309f49d72af
Mike Hill <mikewhill@gmail.com>
2013-10-19 17:50:47 +0100

    2) Added DelegatingResultResponder

---
+++ src/fitnesse/responders/search/DelegatingResultResponder.java
@@ -0,0 +1,21 @@
+package fitnesse.responders.search;
+
+import fitnesse.components.TraversalListener;
+
+public class DelegatingResultResponder extends ResultResponder {
+    private final ResultResponder resultResponder;
+
+    public DelegatingResultResponder(ResultResponder resultResponder) {
+        this.resultResponder = resultResponder;
+    }
+
+    @Override
+    protected String getTitle() {
+        return resultResponder.getTitle();
+    }
+
+    @Override
+    public void traverse(TraversalListener<Object> observer) {
+        resultResponder.traverse(observer);
+    }
+}
